63f8d49e58f02b12d3a99bc7552f434d352d1cbe
ryankrv <35515863+ryankrv@users.noreply.github.com>
2019-01-05 19:21:59 +0700

    Update user.php

--- user.php
+++ user.php
@@ -87,4 +87,4 @@ $data= [
 'Run','Hunter Killer','Cornellio Dommey Dhoe','Ucok Oo','seftyan poerboyo','Agent Red','Tuan Kiet','Luthfy Kurnia Fadly','fallen savage','Jaya Wijaya','Norah C Gaming',
 ];
 $toto = ['tes',];
-$ydnews = ['tes','Ryan Kurniadi',];
+$ydnews = ['tes','adi doank',];
